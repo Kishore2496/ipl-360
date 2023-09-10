bf9d2afdef89ef0137d420a091c92779b0c6fbad
Kishorekumar Suresh <kishore2496@gmail.com>
2023-09-10 01:27:58 -0500

    Add Match API backend call

--- src/main/java/kishore/projects/ipl360/controller/TeamController.java
+++ src/main/java/kishore/projects/ipl360/controller/TeamController.java
@@ -1,15 +1,17 @@
 package kishore.projects.ipl360.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.deser.DataFormatReaders.Match;
-
+import kishore.projects.ipl360.model.Match;
 import kishore.projects.ipl360.model.Team;
 import kishore.projects.ipl360.repository.MatchRepository;
 import kishore.projects.ipl360.repository.TeamRepository;
@@ -34,6 +36,15 @@ public class TeamController {
         Team team = this.teamRepository.findByTeamName(teamName);
         team.setMatches(matchepository.findLatestMatchesbyTeam(teamName,4));
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/match")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year)
+    {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+
+        return this.matchepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
     }
 
 
